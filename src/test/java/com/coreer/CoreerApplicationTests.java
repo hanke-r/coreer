61dafb6a739c00892bb9deb245f01544a3a26fdc
hanjaeok17 <hanjaeok17@illuvation.kr>
2021-05-23 00:29:51 +0900

    Initial commit

---
+++ src/test/java/com/coreer/CoreerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coreer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
